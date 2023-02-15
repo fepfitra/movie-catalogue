91b042ebee5f3026752ca9653a73f50cd9f17585
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2023-02-15 05:33:15 -0500

    membuat application shell

---
+++ src/scripts/apps.js
@@ -0,0 +1,15 @@
+class App {
+  constructor({ button, drawer, content }) {
+    this._button = button;
+    this._drawer = drawer;
+    this._content = content;
+ 
+    this._initialAppShell();
+  }
+ 
+ 
+  _initialAppShell() {
+    // TODO: initial Drawer
+    // kita bisa menginisiasikan komponen lain bila ada
+  }
+}
