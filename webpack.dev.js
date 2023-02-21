27f47d8691ab1897d1791a0152daa779ada92e41
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2023-02-21 22:49:41 +0700

    fix build-rerve error

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,6 @@
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
 const { merge } = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
+const path = require('path');
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'production',
