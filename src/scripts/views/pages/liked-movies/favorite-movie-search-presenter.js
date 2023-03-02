c68c7eae73bf04ad5e3fca39dc8bebf97ad74d60
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2023-03-02 08:35:10 +0700

    rahasia mata-mata

--- src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
+++ src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
@@ -11,12 +11,14 @@ class FavoriteMovieSearchPresenter {
     });
   }
 
-  _searchMovies(latestQuery) {
+  async _searchMovies(latestQuery) {
     this._latestQuery = latestQuery;
+    const foundMovies = await this._favoriteMovies.searchMovies(this.latestQuery);
     this._favoriteMovies.searchMovies(this.latestQuery);
   }
 
   _showFoundMovies(movies) {
+    console.log(movies);
     const html = movies.reduce(
       (carry, movie) => carry.concat(`<li class="movie">
         <span class="movie__title">${movie.title || '-'}</span>
@@ -24,6 +26,8 @@ class FavoriteMovieSearchPresenter {
       '',
     );
     document.querySelector('.movies').innerHTML = html;
+    document.getElementById('movie-search-container')
+      .dispatchEvent(new Event('movies:searched:updated'));
   }
         
   get latestQuery() {
