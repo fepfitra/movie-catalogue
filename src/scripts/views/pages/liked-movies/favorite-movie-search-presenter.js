54edb8ec385f4c0ed2e33e82e49d6c14676b1402
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2023-03-03 10:27:57 +0700

    prepare for fix

--- src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
+++ src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
@@ -1,41 +1,31 @@
 class FavoriteMovieSearchPresenter {
-  constructor({ favoriteMovies }) {
+  constructor({ favoriteMovies, view }) {
+    this._view = view;
     this._listenToSearchRequestByUser();
     this._favoriteMovies = favoriteMovies;
   }
+
   _listenToSearchRequestByUser() {
-    this._queryElement = document.getElementById('query');
-    this._queryElement.addEventListener('change', (event) => {
-      this._searchMovies(event.target.value);
+    this._view.runWhenUserIsSearching((latestQuery) => {
+      this._searchMovies(latestQuery);
     });
   }
-
+  
   async _searchMovies(latestQuery) {
     this._latestQuery = latestQuery.trim();
-
     let foundMovies;
     if (this.latestQuery.length > 0) {
       foundMovies = await this._favoriteMovies.searchMovies(this.latestQuery);
     } else {
       foundMovies = await this._favoriteMovies.getAllMovies();
     }
-
     this._showFoundMovies(foundMovies);
   }
+
   _showFoundMovies(movies) {
-    let html;
-    if (movies.length > 0) {
-      html = movies.reduce(
-        (carry, movie) => carry.concat(`<li class="movie"><span class="movie__title">${movie.title || '-'}</span></li>`),
-        '',
-      );
-    } else {
-      html = '<div class="movies__not__found">Film tidak ditemukan</div>';
-    };
-    document.querySelector('.movies').innerHTML = html;
-    document.getElementById('movie-search-container')
-      .dispatchEvent(new Event('movies:searched:updated'));
+    this._view.showMovies(movies);
   }
+
   get latestQuery() {
     return this._latestQuery;
   }
