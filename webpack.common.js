93305029c257ae72feb7dc0111fe20834a62bec5
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2023-03-04 16:17:59 +0700

    Kenalkan saya codecept

--- webpack.common.js
+++ webpack.common.js
@@ -1,16 +1,13 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: {
-    app: path.resolve(__dirname, 'src/scripts/index.js'),
-    sw: path.resolve(__dirname, 'src/scripts/sw.js'),
-  },
+  entry: path.resolve(__dirname, 'src/scripts/index.js'),
   output: {
-    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
+    filename: 'bundle.js',
   },
   module: {
     rules: [
@@ -29,8 +26,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
       template: path.resolve(__dirname, 'src/templates/index.html'),
+      filename: 'index.html',
     }),
     new CopyWebpackPlugin({
       patterns: [
@@ -39,6 +36,9 @@ module.exports = {
           to: path.resolve(__dirname, 'dist/'),
         },
       ],
+    }),
+    new ServiceWorkerWebpackPlugin({
+      entry: path.resolve(__dirname, 'src/scripts/sw.js'),
     }),
   ],
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,20 +1,10 @@
+const { merge } = require('webpack-merge');
 const path = require('path');
 const common = require('./webpack.common');
-const { merge } = require('webpack-merge');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'inline-source-map',
   devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    open: true,
-    port: 9000,
-    client: {
-      overlay: {
-        errors: true,
-        warnings: true,
-      },
-    },
-    compress: true,
+    contentBase: path.resolve(__dirname, 'dist'),
   },
 });
