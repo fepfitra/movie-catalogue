dbc4c7618306a10f121922461e9b6c58da26cd58
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2023-03-04 14:43:39 +0700

    memperbaiki dia yang terlupakan

--- specs/favoriteMovieSearchSpec.js
+++ specs/favoriteMovieSearchSpec.js
@@ -48,7 +48,7 @@ describe('Searching movies', () => {
 
     it('should show the found movies', () => {
       presenter._showFoundMovies([{ id: 1 }]);
-      expect(document.querySelectorAll('.movie').length)
+      expect(document.querySelectorAll('.movie-item').length)
         .toEqual(1);
 
       presenter._showFoundMovies([{
@@ -58,7 +58,7 @@ describe('Searching movies', () => {
         id: 2,
         title: 'Dua',
       }]);
-      expect(document.querySelectorAll('.movie').length)
+      expect(document.querySelectorAll('.movie-item').length)
         .toEqual(2);
     });
 
@@ -73,7 +73,7 @@ describe('Searching movies', () => {
     });
 
     it('should show - when the movie returned does not contain a title', (done) => {
-      document.getElementById('movie-search-container').addEventListener('movies:searched:updated', () => {
+      document.getElementById('movies').addEventListener('movies:updated', () => {
         const movieTitles = document.querySelectorAll('.movie__title');
         expect(movieTitles.item(0).textContent).toEqual('-');
 
@@ -117,24 +117,21 @@ describe('Searching movies', () => {
 
   describe('When no favorite movies could be found', () => {
     it('should show the empty message', (done) => {
-      document.getElementById('movie-search-container')
-        .addEventListener('movies:searched:updated', () => {
-          expect(document.querySelectorAll('.movies__not__found').length)
-            .toEqual(1);
-          done();
+      document.getElementById('movies').addEventListener('movies:updated', () => {
+        expect(document.querySelectorAll('.movie-item__not__found').length)
+          .toEqual(1);
+        done();
         });
 
-      favoriteMovies.searchMovies.withArgs('film a')
-        .and
-        .returnValues([]);
+      favoriteMovies.searchMovies.withArgs('film a').and.returnValues([]);
 
       searchMovies('film a');
     });
 
     it('should not show any movie', (done) => {
-      document.getElementById('movie-search-container')
-        .addEventListener('movies:searched:updated', () => {
-          expect(document.querySelectorAll('.movie').length)
+      document.getElementById('movies')
+        .addEventListener('movies:updated', () => {
+          expect(document.querySelectorAll('.movie-item').length)
             .toEqual(0);
           done();
         });
